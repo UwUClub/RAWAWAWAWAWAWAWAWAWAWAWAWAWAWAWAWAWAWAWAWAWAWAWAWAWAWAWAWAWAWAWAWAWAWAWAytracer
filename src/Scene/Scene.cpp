f2152e926f46fe1c75311703f4613af0d0740962
Valegox <valentin.gegoux@epitech.eu>
2023-04-26 12:30:44 +0200

    Implement createPPM method

--- src/Scene/Scene.cpp
+++ src/Scene/Scene.cpp
@@ -4,6 +4,10 @@
 
 #include "Scene.hpp"
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <string>
 
 namespace RayTracer::Scene
 {
@@ -39,5 +43,28 @@ namespace RayTracer::Scene
         if (_entities["Light"].empty())
             throw SceneException("No light in the scene");
         return _entities["Light"];
+    }
+
+    void Scene::createPPM(const std::string &aFileName, const std::vector<Color> &aPixels, const int aWidth, const int aHeight)
+    {
+        std::ofstream file(aFileName);
+        if (!file.is_open())
+            throw SceneException("Cannot open file " + aFileName);
+        file << "P3\n" << aWidth << " " << aHeight << "\n255\n";
+
+        int col = 1;
+        for (const auto &pixel : aPixels) {
+            file << std::setw(3) << pixel._r << " ";
+            file << std::setw(3) << pixel._g << " ";
+            file << std::setw(3) << pixel._b;
+            if (col == aWidth) {
+                file << "\n";
+                col = 0;
+            } else {
+                file << " ";
+            }
+            col++;
+        }
+        file.close();
     }
 } // namespace RayTracer::Scene
