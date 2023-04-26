e3b5150b83bc07407195ea4752aa7156b0c0510b
Baptiste Laran <baptiste.laran@epitech.eu>
2023-04-26 15:25:46 +0200

    Fix getClosestHit function

--- src/Utils/Ray.cpp
+++ src/Utils/Ray.cpp
@@ -23,12 +23,16 @@ namespace RayTracer {
         double closest = 0;
         std::optional<double> newDist;
     
-        for (auto &entity : entities["Primitives"]) {
-            Entity::Primitives *prim = static_cast<Entity::Primitives *>(entity.get());
-            newDist = prim->isTouched(*this);
-            if (closest == 0 || (newDist != std::nullopt && newDist.value() < closest)) {
-                closest = newDist.value();
-                color = prim->getColor();
+        for (auto &entity : entities) {
+            if (entity.first == "Sphere" || entity.first == "Plane" || entity.first == "Cylinder" || entity.first == "Cone") {
+                for (auto &entity2 : entity.second) {
+                    Entity::Primitives *prim = static_cast<Entity::Primitives *>(entity2.get());
+                    newDist = prim->isTouched(*this);
+                    if (closest == 0 || (newDist != std::nullopt && newDist.value() < closest)) {
+                        closest = newDist.value();
+                        color = prim->getColor();
+                    }
+                }
             }
         }
         return color;
