0964a0dfab7734dfd1689ed2b05d23b601f3f8a5
Valegox <valentin.gegoux@epitech.eu>
2023-05-03 22:09:45 +0200

    Added main logic

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,11 +2,18 @@
 // Created by patatofour on 14/04/23.
 //
 
+#include "Entity/Entity.hpp"
+#include "HitPoint.hpp"
 #include "IEntity.hpp"
 #include "PluginManager.hpp"
 #include "PluginObserver.hpp"
 #include "Scene.hpp"
 #include "Parser.hpp"
+#include "Primitives.hpp"
+#include "Camera.hpp"
+#include "Ray.hpp"
+#include <memory>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
@@ -23,8 +30,22 @@ int main(int argc, char *argv[])
     try {
         RayTracer::Parser::Parser parser(argv, scene, pluginManager);
         pluginObserver.subscribe(pluginManager);
-        while (true) {
-            pluginObserver.checkPlugins(entityMap);
+        pluginObserver.checkPlugins(entityMap);
+
+        // Get cam rays
+        auto &myCam = scene.getCamera();
+        const std::vector<RayTracer::Ray> myRays = myCam->getCastedRays();
+
+        std::cout << "nb of rays: " << myRays.size() << std::endl;
+
+        // Send rays
+        for (auto ray : myRays) {
+            auto hit = ray.getClosestHit(entityMap);
+            auto color = RayTracer::Color(0, 0, 0);
+            if (hit) {
+                color = hit->calcColor(entityMap);
+            }
+            std::cout << color._r << color._g << color._b << std::endl;
         }
     } catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
